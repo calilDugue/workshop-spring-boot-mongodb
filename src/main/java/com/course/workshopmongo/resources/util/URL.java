fd74dcf9acd86ac05f51643039969b6739b0fea1
calilDugue <calildugue@hotmail.com>
2025-04-05 14:54:46 -0300

    Consulta com vários critérios

--- src/main/java/com/course/workshopmongo/resources/util/URL.java
+++ src/main/java/com/course/workshopmongo/resources/util/URL.java
@@ -1,6 +1,9 @@
 package com.course.workshopmongo.resources.util;
 
 import java.net.URLDecoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
     public static String decodeParam(String text) {
@@ -8,6 +11,17 @@ public class URL {
             return URLDecoder.decode(text, "UTF-8");
         } catch (Exception e) {
             return "";
+        }
+    }
+
+    public static Date convertDate(String textDate, Date defaultValue) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        try {
+            return sdf.parse(textDate);
+        } catch (Exception e) {
+            return defaultValue;
         }
     }
 }
